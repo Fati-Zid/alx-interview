7b2fa5674ea4fc9be5146f94e4b101b6e56d8fb5
Fati-Zid <ezzaidanif@gmail.com>
2024-06-21 21:50:28 +0100

    restyling: Verefy PEP 8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 """
-  Returns a list of lists of integers representing the Pascal’s triangle of n :
-  - Returns an empty list if n <= 0.
-  - Assume that n will be always an integer.
-  - hint: 
-      -- Each element in the middle is the sum of the two elements above it.
+Returns a list of lists of integers representing the Pascal’s triangle of n:
+- Returns an empty list if n <= 0.
+- Assume that n will be always an integer.
+- Hint:
+  -- Each element in the middle is the sum of the two elements above it.
 """
 
-def pascal_triangle(n):
-  """  Return an empty list if n <= 0 """
-  if(n<= 0):
-    return []
-  """ start the first row by 1"""
-  pascal_triangle = [[1]]
 
-  """ Build other rows: using row i and column j """
-  for i in range(1, n):
-    row = [1] # <= first row
-    for j in range(1, i):
-      """ Calculate the midle element"""
-      row.append(pascal_triangle[i-1][j-1] + pascal_triangle[i-1][j])
-    row.append(1) # <= end of the row
-    pascal_triangle.append(row)
-  
-  return pascal_triangle
+def pascal_triangle(n):
+    """Return a list of lists representing Pascal's triangle of n."""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [[1]]  # Start the first row with 1
+
+    for i in range(1, n):
+        row = [1]  # First element of the row is always 1
+        for j in range(1, i):
+            row.append(pascal_triangle[i - 1][j - 1]+pascal_triangle[i - 1][j])
+        row.append(1)  # Last element of the row is always 1
+        pascal_triangle.append(row)
+
+    return pascal_triangle
